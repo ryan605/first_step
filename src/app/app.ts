89a9a56b5ce68089c304863eb4906f008b836d31
RYAN LANGAT <LANGAT@Mac.mshome.net>
2025-11-12 19:33:45 +0300

    changes to user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
+import {Users} from './users/users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Users],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
